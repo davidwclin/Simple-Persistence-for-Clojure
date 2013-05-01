86225b5687092d804ff4fbb18644b3f5ba0dbb8b
davidwclin <davidwclin@gmail.com>
2013-05-01 16:25:16 -0700

    add groupId

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject persister "0.0.1"
+(defproject davidwclin/persister "0.0.1"
   :description "Simple Persistence for Clojure"
   :url "https://github.com/davidwclin/Simple-Persistence-for-Clojure" 
   :dependencies [[org.clojure/clojure "1.4.0"]
